b6868de5be156b12e160d8f0e4840527397c2168
Bhavesh <bhaveshkapoor09@gmail.com>
2025-05-27 12:14:58 +0530

    demo commit

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 class MessageController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource. 
      */
     public function index()
     {
